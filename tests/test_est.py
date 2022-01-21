eb8e3d2af49c44f1ecfc4bd9e050447b04a113b3
shawnlimjunhe <46809501+shawnlimjunhe@users.noreply.github.com>
2022-01-21 13:45:03 +0800

    boilerplate for est tests

---
+++ tests/test_est.py
@@ -0,0 +1,27 @@
+import pytest
+from commands import est
+
+
+@pytest.mark.skip(reason="yet to implement")
+def test_est():
+    pass
+
+
+@pytest.mark.skip(reason="yet to implement")
+def test_est_reach():
+    pass
+
+
+@pytest.mark.skip(reason="yet to implement")
+def test_est_travel():
+    pass
+
+
+@pytest.mark.skip(reason="yet to implement")
+def test_est_skip_ready():
+    pass
+
+
+@pytest.mark.skip(reason="yet to implement")
+def test_est_ready():
+    pass
