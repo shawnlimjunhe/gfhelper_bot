3b71a2bd20cb0ca050eaad6f5549d50398d22ef4
shawnlimjunhe <46809501+shawnlimjunhe@users.noreply.github.com>
2022-01-21 13:22:52 +0800

    cleaned up help tests

--- tests/test_help.py
+++ tests/test_help.py
@@ -1,32 +1,17 @@
-from distutils import command
 import pytest
-from unittest import mock
-from commands.help import help, help_default_txt, command_mapping, invalid_command_txt
-# test 1 replys with expected help message when no context args
-
-
-@pytest.fixture
-def mocked_update():
-    mocked_update = mock.Mock()
-    mocked_update.effective_chat.id = 0
-    return mocked_update
-
-
-@pytest.fixture
-def mocked_context():
-    return mock.Mock()
+from commands import help
 
 
 @pytest.mark.parametrize(
     "context_args, expected",
     [
-        (None, help_default_txt),
-        (['est'], command_mapping['est']),
-        (['sleep'], command_mapping['sleep']),
+        (None, help.help_default_txt),
+        (['est'], help.command_mapping['est']),
+        (['sleep'], help.command_mapping['sleep']),
         # failure cases
-        ([''], invalid_command_txt),
-        (['est', 'random'], command_mapping['est']),
-        ([], help_default_txt)
+        ([''], help.invalid_command_txt),
+        (['est', 'random'], help.command_mapping['est']),
+        ([], help.help_default_txt)
     ],
     ids=[
         'help command default behavior',
@@ -41,7 +26,7 @@ def test_help(mocked_update, mocked_context, context_args, expected):
     mocked_update.message.text = "test message"
     mocked_context.args = context_args
 
-    help(mocked_update, mocked_context)
+    help.help(mocked_update, mocked_context)
 
     mocked_context.bot.send_message.assert_called()
     mocked_context.bot.send_message.assert_called_with(
