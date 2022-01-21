5d4b2071da87fd27a7a9b7490896f89db895d981
shawnlimjunhe <46809501+shawnlimjunhe@users.noreply.github.com>
2022-01-21 13:43:15 +0800

    added boilplate for 'sleep' and 'est' tests

--- tests/test_sleep.py
+++ tests/test_sleep.py
@@ -1,1 +1,37 @@
-from unittest import mock
+import pytest
+from commands import sleep
+
+
+@pytest.mark.skip(reason="yet to implement")
+def test_sleep_time_arr_to_str():
+    pass
+
+
+@pytest.mark.skip(reason="yet to implement")
+def test_get_sleeptimes_from_wake():
+    pass
+
+
+@pytest.mark.skip(reason="yet to implement")
+def test_process_sleep_times_text():
+    pass
+
+
+@pytest.mark.skip(reason="yet to implement")
+def test_sleep():
+    pass
+
+
+@pytest.mark.skip(reason="yet to implement")
+def test_sleep_now():
+    pass
+
+
+@pytest.mark.skip(reason="yet to implement")
+def test_sleep_wake():
+    pass
+
+
+@pytest.mark.skip(reason="yet to implement")
+def test_sleep_wake_time():
+    pass
